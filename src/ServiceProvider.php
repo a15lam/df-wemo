43a3814cde8327df256a09791c9d47e99f69e9e7
Arif Islam <arif@dreamfactory.com>
2017-03-02 02:38:34 -0500

    updates

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,27 +14,8 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     use ServiceDocBuilder;
 
-    public function register()
+    public function boot()
     {
-        // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
-            $df->addType(
-                new ServiceType([
-                    'name'            => 'wemo',
-                    'label'           => 'Wemo',
-                    'description'     => 'Belkin Wemo service.',
-                    'group'           => 'IOT',
-                    'config_handler'  => null,
-                    'default_api_doc' => function ($service){
-                        return $this->buildServiceDoc($service->id, Wemo::getApiDocInfo($service));
-                    },
-                    'factory'         => function ($config){
-                        return new Wemo($config);
-                    },
-                ])
-            );
-        });
-
         // Auto seed the service as it doesn't requir any configuration.
         // Default service name is 'wemo'. You can change this using
         // WEMO_SERVICE_NAME Environment option.
@@ -58,5 +39,27 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                 Cache::forever('wemo-seeded', true);
             }
         }
+    }
+
+    public function register()
+    {
+        // Add our service types.
+        $this->app->resolving('df.service', function (ServiceManager $df){
+            $df->addType(
+                new ServiceType([
+                    'name'            => 'wemo',
+                    'label'           => 'Wemo',
+                    'description'     => 'Belkin Wemo service.',
+                    'group'           => 'IOT',
+                    'config_handler'  => null,
+                    'default_api_doc' => function ($service){
+                        return $this->buildServiceDoc($service->id, Wemo::getApiDocInfo($service));
+                    },
+                    'factory'         => function ($config){
+                        return new Wemo($config);
+                    },
+                ])
+            );
+        });
     }
 }
