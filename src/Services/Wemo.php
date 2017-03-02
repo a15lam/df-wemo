1fc4092ec20285f7869486f52052328b917822b2
Arif Islam <arif@dreamfactory.com>
2017-03-02 02:01:58 -0500

    updates

--- src/Services/Wemo.php
+++ src/Services/Wemo.php
@@ -14,6 +14,13 @@ class Wemo extends BaseRestService
     const DEVICE_CACHE_FILE = 'storage/app/wemo-device.json';
 
     /** {@inheritdoc} */
+    public function __construct(array $settings = [])
+    {
+        parent::__construct($settings);
+        Discovery::$deviceFile = base_path(static::DEVICE_CACHE_FILE);
+    }
+
+    /** {@inheritdoc} */
     protected function handleGET()
     {
         if ($this->resource) {
@@ -68,7 +75,6 @@ class Wemo extends BaseRestService
     protected function findDevices($refresh = false, $asList = false)
     {
         $list = [];
-        Discovery::$deviceFile = base_path(static::DEVICE_CACHE_FILE);
         $devices = Discovery::find($refresh);
         foreach ($devices as $device) {
             $model = array_get($device, 'modelName');
@@ -132,5 +138,153 @@ class Wemo extends BaseRestService
         } else {
             throw new InternalServerErrorException('Unsupported device [' . $id . ']');
         }
+    }
+
+    /** @inheritdoc */
+    public static function getApiDocInfo($service)
+    {
+        $serviceName = strtolower($service->name);
+
+        $base = [
+            'paths' => [
+                '/' . $serviceName                                      => [
+                    'get' => [
+                        'tags'        => [$serviceName],
+                        'summary'     => 'getDevices() - Get device list',
+                        'operationId' => 'getDevices',
+                        'consumes'    => ['application/json', 'application/xml'],
+                        'produces'    => ['application/json', 'application/xml'],
+                        'description' => 'Fetches device list',
+                        'parameters'  => [
+                            [
+                                'name'        => 'refresh',
+                                'type'        => 'boolean',
+                                'description' => 'Force device discovery',
+                                'in'          => 'query',
+                                'required'    => false,
+                            ],
+                            [
+                                'name'        => 'as_list',
+                                'type'        => 'boolean',
+                                'description' => 'Returns a plain list of devices',
+                                'in'          => 'query',
+                                'required'    => false,
+                            ],
+                        ],
+                        'responses'   => [
+                            '200'     => [
+                                'description' => 'Success',
+                                'schema'      => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'device' => [
+                                            'type'  => 'array',
+                                            'items' => [
+                                                'type'       => 'object',
+                                                'properties' => [
+                                                    'id'       => ['type' => 'string'],
+                                                    'dimmable' => ['type' => 'boolean']
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ],
+                            'default' => [
+                                'description' => 'Error',
+                                'schema'      => ['$ref' => '#/definitions/Error']
+                            ]
+                        ],
+                    ]
+                ],
+                '/' . $serviceName . '/{device_id}'                     => [
+                    'get' => [
+                        'tags'        => [$serviceName],
+                        'summary'     => 'getDeviceState() - Get device state',
+                        'operationId' => 'getDeviceState',
+                        'consumes'    => ['application/json', 'application/xml'],
+                        'produces'    => ['application/json', 'application/xml'],
+                        'description' => 'Fetches device state',
+                        'parameters'  => [
+                            [
+                                'name'        => 'device_id',
+                                'type'        => 'string',
+                                'description' => 'ID of the device to operate on',
+                                'in'          => 'path',
+                                'required'    => true,
+                            ],
+                        ],
+                        'responses'   => [
+                            '200'     => [
+                                'description' => 'Success',
+                                'schema'      => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'state'     => ['type' => 'boolean'],
+                                        'dim_level' => ['type' => 'integer']
+                                    ]
+                                ]
+                            ],
+                            'default' => [
+                                'description' => 'Error',
+                                'schema'      => ['$ref' => '#/definitions/Error']
+                            ]
+                        ],
+                    ]
+                ],
+                '/' . $serviceName . '/{device_id}/{state}/{dim_level}' => [
+                    'get' => [
+                        'tags'        => [$serviceName],
+                        'summary'     => 'setDeviceState() - Set device state',
+                        'operationId' => 'setDeviceState',
+                        'consumes'    => ['application/json', 'application/xml'],
+                        'produces'    => ['application/json', 'application/xml'],
+                        'description' => 'Sets device state',
+                        'parameters'  => [
+                            [
+                                'name'        => 'device_id',
+                                'type'        => 'string',
+                                'description' => 'ID of the device to operate on',
+                                'in'          => 'path',
+                                'required'    => true,
+                            ],
+                            [
+                                'name'        => 'state',
+                                'type'        => 'string',
+                                'description' => 'State of the device. Available options are on,off,dim',
+                                'in'          => 'path',
+                                'required'    => true,
+                            ],
+                            [
+                                'name'        => 'dim_level',
+                                'type'        => 'integer',
+                                'description' => 'Dim level in percentage. Available options are 0% - 100%. Default is 50%.',
+                                'in'          => 'path',
+                                'required'    => false,
+                            ],
+                        ],
+                        'responses'   => [
+                            '200'     => [
+                                'description' => 'Success',
+                                'schema'      => [
+                                    'type'       => 'object',
+                                    'properties' => [
+                                        'state'     => ['type' => 'boolean'],
+                                        'dim_level' => ['type' => 'integer']
+                                    ]
+                                ]
+                            ],
+                            'default' => [
+                                'description' => 'Error',
+                                'schema'      => ['$ref' => '#/definitions/Error']
+                            ]
+                        ],
+                    ]
+                ]
+            ],
+
+        ];
+
+        return $base;
     }
 }
